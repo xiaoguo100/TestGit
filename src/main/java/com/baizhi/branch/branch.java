6aea4ab0dba3d5d0ccc14b5e80a6bbb2023776f3
xiaoming <490241235@qq.com>
2018-06-09 17:10:16 +0800

    newBranch分支的第一次提交

---
+++ src/main/java/com/baizhi/branch/branch.java
@@ -0,0 +1,10 @@
+package com.baizhi.branch;
+
+/**
+ * Created by guoming100 on 2018/6/9.
+ */
+public class branch {
+    public static void main(String[] args) {
+        System.out.println("我是newBranch分支的主方法");
+    }
+}
