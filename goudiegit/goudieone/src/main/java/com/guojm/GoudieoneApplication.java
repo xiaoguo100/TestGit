3a826f88cfc31b4e2ba0d5428c5adafe68fa3aed
gjm <490241235@qq.com>
2019-01-04 10:23:00 +0800

    1

---
+++ goudiegit/goudieone/src/main/java/com/guojm/GoudieoneApplication.java
@@ -0,0 +1,14 @@
+package com.guojm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoudieoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoudieoneApplication.class, args);
+	}
+
+}
+
